450ff14abdffa17e5f852ad4a945f5a0748183ec
Zynalex9 <alzn952@gmail.com>
2024-11-20 13:50:26 +0500

    Modify user model

--- src/models/userModel.model.js
+++ src/models/userModel.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
@@ -19,12 +20,43 @@ const userSchema = new Schema(
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters long"],
     },
-    todos: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Todo",
-    },]
+    todos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Todo",
+      },
+    ],
   },
-  { timestamps: true }
+  { timestamps: true },
 );
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: process.env.JWT_EXPIRY,
+    },
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: process.env.REFRESH_EXPIRY,
+    },
+  );
+};
 const userModel = mongoose.model("User", userSchema);
-export default  userModel
+export default userModel;
