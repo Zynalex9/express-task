1a89d2fedeb3e4ff00d8dfeecca847b348f3a49f
Zynalex9 <alzn952@gmail.com>
2024-11-23 11:03:44 +0500

    Basic React app with backend connection using proxy

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ app.use(cookieParser());
 
 import { userRouter } from "./routes/user.route.js";
 import { todosRouter } from "./routes/todos.route.js";
-app.use("/user", userRouter);
-app.use("/task", todosRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/task", todosRouter);
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    { title: "hello 1", desc: "mellow 1" },
+    { title: "hello 2", desc: "mellow 2" },
+    { title: "hello 3", desc: "mellow 3" },
+  ];
+  res.send(jokes);
+});
 export { app };
